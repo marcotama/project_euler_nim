763a2ebc1b3b8812af4e5c6dada1ebc948afebe9
Marco Tamassia <tamassia.marco@gmail.com>
2017-08-15 14:52:44 +0900

    Solve problem 010

---
+++ euler_010_summation_of_primes.nim
@@ -0,0 +1,32 @@
+from strutils import parseInt
+from math import sqrt
+
+proc computeSummationOfPrimes(n: int): int =
+  var
+    primes = @[2]
+    k = 1
+
+  while k <= n - 2:
+    k += 2
+    var prime = true
+    let limit = int(sqrt(float(k)))
+    for d in primes:
+      if d > limit:
+        break
+      if k mod d == 0:
+        prime = false
+        break
+    if prime:
+      primes.add(k)
+
+  for p in primes:
+    result += p
+
+
+
+when isMainModule:
+  let n_test_cases = parseInt(readLine(stdin))
+
+  for i in 1..n_test_cases:
+    let n = parseInt(readLine(stdin))
+    stdout.writeLine($(computeSummationOfPrimes(n)))
