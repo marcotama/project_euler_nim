2ffba316123a789e9efb33125c208e560715773c
Marco Tamassia <tamassia.marco@gmail.com>
2017-08-15 11:34:11 +0900

    Solve question 007

---
+++ euler_007_1001st_prime_number.nim
@@ -0,0 +1,25 @@
+proc computeNthPrime(n: int): int =
+  var
+    primes = @[2]
+    k = 1
+
+  while primes.len < n:
+    k += 2
+    var prime = true
+    for d in primes:
+      if k mod d == 0:
+        prime = false
+        break
+    if prime:
+      primes.add(k)
+  result = k
+
+
+
+when isMainModule:
+  from strutils import parseInt
+  let n_test_cases = parseInt(readLine(stdin))
+
+  for i in 1..n_test_cases:
+    let n = parseInt(readLine(stdin))
+    stdout.writeLine($(computeNthPrime(n)))
