4977a4bb859f52a1f3cca297f2151e6a79392003
Marco Tamassia <tamassia.marco@gmail.com>
2017-08-20 13:22:56 +0900

    Solve challenge 018

---
+++ euler_018_maximum_path_sum_1.nim
@@ -0,0 +1,32 @@
+from strutils import parseInt, split
+import tables
+import future
+from util import max
+
+
+proc calcNumberLetterCount(triangle: seq[seq[int]]): int =
+  var cumulativeTriangle: seq[seq[int]] = @[]
+  cumulativeTriangle.add(@[triangle[0][0]])
+
+  for level in 1..<triangle.len:
+    let
+      prevRow = cumulativeTriangle[level-1]
+      curRow = triangle[level]
+    var cumRow = lc[ curRow[i] | (i <- 0..<curRow.len), int]
+    cumRow[0] += prevRow[0]
+    cumRow[cumRow.len-1] += prevRow[prevRow.len-1]
+    for i in 1..<cumRow.len-1:
+      cumRow[i] += max(prevRow[i-1], prevRow[i])
+    cumulativeTriangle.add(cumRow)
+  return max(cumulativeTriangle[cumulativeTriangle.len-1])
+
+when isMainModule:
+  let nTestCases = parseInt(readLine(stdin))
+
+  for i in 1..nTestCases:
+    let nRows = parseInt(readLine(stdin))
+    var data: seq[seq[int]] = @[]
+    for j in 1..nRows:
+      let line = split(readLine(stdin))
+      data.add(lc[parseInt(n) | (n <- line), int])
+    stdout.writeLine($(calcNumberLetterCount(data)))
