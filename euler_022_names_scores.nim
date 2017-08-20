8ab0b21ab73d2b94f5ec0d593602f3315c806baa
Marco Tamassia <tamassia.marco@gmail.com>
2017-08-20 15:29:37 +0900

    Solve challenge 022

---
+++ euler_022_names_scores.nim
@@ -0,0 +1,18 @@
+from strutils import parseInt
+import algorithm
+import future
+
+proc computeNamesScores(names: seq[string]): int =
+  var names = sorted(names, system.cmp)
+  for i in 0..<names.len:
+    let name = names[i]
+    var nameTot = 0
+    for c in name:
+      nameTot += ord(c) - ord('A') + 1 # let the compiler optimise this
+    result += nameTot * (i + 1)
+
+
+when isMainModule:
+  let nLines = parseInt(readLine(stdin))
+  let data = lc[ stdin.readLine() | (x <- 1..nLines), string]
+  stdout.writeLine($(computeNamesScores(data)))
