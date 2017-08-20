012e1177b7ec16676700369969d076909da14063
Marco Tamassia <tamassia.marco@gmail.com>
2017-08-20 22:17:56 +0900

    Edit output format

--- test_all.sh
+++ test_all.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 for f in $( ls euler_*.nim ); do
-  echo "Testing ${f::-4}"
   ./test_program.sh "${f::-4}"
-  echo ""
 done
--- test_program.sh
+++ test_program.sh
@@ -5,6 +5,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
 
 
 if [[ -e $1 ]]; then
@@ -26,14 +30,15 @@ if [[ -e compile.tmp ]]; then
   rm compile.tmp
 fi
 
-
+COLS=50
 
 { time ./$1 < $1.in &> out.tmp ; } 2> time.tmp
 if [[ $( cat $1.out ) == $( cat out.tmp ) ]]; then
-  printf "${GREEN}Test passed${NC}\n"
-  echo "Elapsed time: " $( cat time.tmp | grep real | grep -oE "[0-9.]+m[0-9.]+s" )
+  TIME=$( cat time.tmp | grep real | grep -oE "[0-9.]+m[0-9.]+s" )
+  SPACES=$( expr $COLS - ${#1} )
+  printf "$1%${SPACES}s${GREEN}Passed${NC} in ${TIME}\n"
 else
-  printf "${RED}Test failed${NC}\n"
+  printf "$1%${SPACES}s${RED}Failed${NC}\n"
   echo ""
   echo "Input:"
   cat $1.in
